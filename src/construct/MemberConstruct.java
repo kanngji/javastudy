fed6d1344ad79bcd12f143f4390286288619b36f
kanngji <alleneffort1994@gmail.com>
2024-09-07 03:25:13 +0900

    this , 기본생성자 , package

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,13 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name, int age){ // 생성자 하나 더 생성 오버로딩
+        this(name,age,50);
+        //this.name = name;
+        //this.age = age;
+        //this.grade= 50;
+    }
+
     MemberConstruct(String name, int age, int grade){ // 생성자 이름은 클래스이름과 같게 해야한다
         System.out.println("생성자 호출= "+name + ", age =" +age + ",grade = "+grade);
         this.name = name;
