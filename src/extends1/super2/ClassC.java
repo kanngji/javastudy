31c9328e4c2a574fd081b50c52f038126c26679d
kanngji <alleneffort1994@gmail.com>
2024-09-21 02:59:48 +0900

    final

--- src/extends1/super2/ClassC.java
+++ src/extends1/super2/ClassC.java
@@ -1,7 +1,10 @@
 package extends1.super2;
 
-//public class ClassC extends ClassB{
+public class ClassC extends ClassB{
 
-
+    public ClassC() {
+        super(50);
+        System.out.println("ClassC 생성자");
+    }
 
 }
